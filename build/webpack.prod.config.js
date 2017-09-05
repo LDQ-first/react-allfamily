e9370e7797e2a417cb38374eb88ec28b0b27a144
LDQ-first <178848890@qq.com>
2017-09-05 23:54:33 +0800

    'modify'

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const webpack = require('webpack')
+const baseConfig = require('./webpack.base.config')
+const merge = require('webpack-merge')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+
+const extractCSS = new ExtractTextPlugin({
+    filename: 'css/main.[name].[contenthash:8].css'
+})
+const extractSASS = new ExtractTextPlugin({
+    filename: 'css/sass.[name].[contenthash:8].css'
+})
+
+module.exports = (baseConfig, {
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            minimize: true //压缩
+                        }
+                    }]
+                })
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/node_modules/, /\.css$/],
+                use: extractSASS.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            minimize: true //压缩
+                        }
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            },
+            {
+            test: /\.(png|svg|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            name: 'img/[name].[sha512:hash:base64:7].[ext]',
+                            minimize: true //压缩
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)((-|\?)?.*)?$/,
+                exclude: /static\/img/,
+                loader: 'url-loader',
+                options: {
+                    name: 'css/font/[name].[hash:8].[ext]',
+                    minimize: true //压缩
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        extractCSS,
+        extractSASS,
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HashedModuleIdsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+           }
+       })
+    ]
+})
+
+
+
