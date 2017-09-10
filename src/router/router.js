95e3e46ff42e9f46221c079bd45a1603f73e8411
LDQ-first <178848890@qq.com>
2017-09-11 02:18:02 +0800

    'modify'

--- src/router/router.js
+++ src/router/router.js
@@ -32,18 +32,6 @@ const createComponent = (component) => () => (
 
 let open = false
 
-/*let home, page1, counter, userinfo, github
-
-
-process.env.NODE_ENV === 'production' 
-     ? ( home =  '/react-allfamily/dist/',
-         page1 = '/react-allfamily/dist/page1',
-         counter = '/react-allfamily/dist/counter',
-         userinfo = '/react-allfamily/dist/userinfo',
-         github = '/react-allfamily/dist/github')
-     : (home =  '/', page1 = '/page1', counter = '/counter', userinfo = '/userinfo', github = '/github' )*/
-
-
 
 const getRouter = () => (
     <ConnectedRouter history={createHistory()}>
