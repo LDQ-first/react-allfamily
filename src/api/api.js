c7858d1ef2993d5f099a9518ffb0230e5ceecc79
LDQ-first <178848890@qq.com>
2017-09-11 10:36:25 +0800

    'modify'

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,7 @@
-const userApi = '/static/json/user.json'
+const userApi = process.env.NODE_ENV === 'production' 
+    ? '/react-allfamily/dist/static/json/proUser.json'
+    : '/static/json/user.json'
+    
 import * as githubApi from './github'
 
 export {
